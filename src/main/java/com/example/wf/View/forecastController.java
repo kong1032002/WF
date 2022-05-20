fb4ac115b5bedb9883fdd2459e65461377eb55fe
NTKong <kong1032002@gmail.com>
2022-05-20 23:38:34 +0700

    them cac loi khuyen

--- src/main/java/com/example/wf/View/forecastController.java
+++ src/main/java/com/example/wf/View/forecastController.java
@@ -48,6 +48,7 @@ public class forecastController implements Initializable {
     public ImageView icon5;
     public Label temp5;
     public Label country;
+    public Label advise;
 
     public void selectCity(ActionEvent actionEvent) {
         String selectedCity = city.getSelectionModel().getSelectedItem();
@@ -56,9 +57,12 @@ public class forecastController implements Initializable {
             return;
         }
         List<DailyWeather> dailyWeather = jsonResult.getDailyWeathers();
+
         temperature.setText(dailyWeather.get(1).getMain().getTemp() + " °C");
         wind.setText("Gió: " + dailyWeather.get(1).getWinds().getSpeed() + "m/s");
-        date.setText(String.valueOf(dailyWeather.get(1).getDt_txt()));
+        String today = dailyWeather.get(1).getDt_txt();
+        String[] time = today.split(" ");
+        date.setText(today);
         weather.setText(dailyWeather.get(1).getWeather().getMain());
         description.setText(dailyWeather.get(1).getWeather().getDescription());
         humidity.setText("Độ ẩm: " + dailyWeather.get(1).getMain().getHumidity() + "%");
@@ -70,11 +74,26 @@ public class forecastController implements Initializable {
         day3.setText(DOW[(++now - 1) % 7]);
         day4.setText(DOW[(++now - 1) % 7]);
         day5.setText(DOW[(++now - 1) % 7]);
-//        switch (weather.getText()) {
-//            case "Rain" -> advise.setText("Bạn nên phơi đồ trong nhà, ra ngoài nhớ mang theo ô nhé!");
-//            case "Clear" -> advise.setText("Trời quang, thích hợp phơi đồ, nên thoa kem chống nắng khi ra ngoài khi trời sáng");
-//            case "Cloud" -> advise.setText("Trời có mây!!!!");
-//        }
+        if (Integer.parseInt(time[1].substring(0, 2)) < 18) {
+            switch (weather.getText()) {
+                case "Rain" -> {
+                    switch (description.getStyle()) {
+                        case "heavy intensity rain" -> advise.setText("Mưa to, mưa rào lớn, gia cố, che chắn các cửa số để tránh bị nước hắt vào, hạn chế ra ngoài nhé");
+                        case "light rain" -> advise.setText("Mưa nhỏ, bạn nghĩ sao nếu ngắm mưa với một cốc cafe và một quyển sách");
+                        default -> advise.setText("Bạn nên phơi đồ trong nhà, ra ngoài nhớ mang theo ô nhé!");
+                    }
+                }
+                case "Clear" -> advise.setText("Trời quang, thích hợp phơi đồ, nên thoa kem chống nắng khi ra ngoài khi trời sáng");
+                case "Clouds" -> {
+                    switch (description.getText()) {
+                        case "overcast clouds" -> advise.setText("Trời u ám có thể có mưa ra ngoài nên mang theo ô để đề phòng nhé");
+                        case "few clouds" -> advise.setText("Trời quang, mây tạnh, ra ngoài nhớ đội mũ nhé");
+                        case "scattered clouds", "broken clouds" -> advise.setText("Mây rải rác!!!!!!!!!!!");
+                        default -> advise.setText("Trời có mây, phù hợp cho các hoạt động thể thao ngoài trời!!!!");
+                    }
+                }
+            }
+        }
         temp1.setText(dailyWeather.get(9).getMain().getTemp() + " °C");
         temp2.setText(dailyWeather.get(17).getMain().getTemp() + " °C");
         temp3.setText(dailyWeather.get(26).getMain().getTemp() + " °C");
